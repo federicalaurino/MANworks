ea0cd3e9ce2d911b5fa35468431e0038e1d86e02
Stefano Brambilla <s.brambilla93@gmail.com>
2017-01-19 17:38:20 +0100

    DIFFUSION PROBLEM: NO NEED FOR NOTARO SOLVER

--- src/8_network_transp/main.cpp
+++ src/8_network_transp/main.cpp
@@ -13,7 +13,7 @@
   @date   January 2017. 
   @brief  Main program for test simulations.    
   @details
-    We solve the coupled 3D/1D problem of fluid exchange between a 1D  
+    We solve the coupled 3D/1D problem of fluid exchange between a 1D   
     network \Lambda and the 3D interstitial tissue \Omega
           
     ***************************************** 
@@ -49,17 +49,18 @@ int main(int argc, char *argv[])
 		// Build the monolithic system 
 		p.problem3d1d::assembly();
 		// Solve the problem 
-		if (!p.problem3d1d::solve()) GMM_ASSERT1(false, "solve procedure has failed");
+		//if (!p.problem3d1d::solve()) GMM_ASSERT1(false, "solve procedure has failed");
 		// Save results in .vtk format
-		p.problem3d1d::export_vtk();
+		//p.problem3d1d::export_vtk();
 		// Display some global results: mean pressures, total flow rate
 		
+		/*
 		std::cout << "--- FINAL RESULTS -------------------------" << std::endl; 
 		std::cout << "  Pt average            = " << p.mean_pt()   << std::endl;
 		std::cout << "  Pv average            = " << p.mean_pv()   << std::endl;
 		std::cout << "  Network-to-Tissue TFR = " << p.flow_rate() << std::endl;
 		std::cout << "-------------------------------------------" << std::endl; 	
-		
+		*/
 		
 		     
 		//transport problem: concentration  
