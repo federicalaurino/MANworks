4ab598f00eabbee8140b28a4f33ac10b5c5bdc3d
Stefano Brambilla <s.brambilla93@gmail.com>
2018-06-05 07:52:45 +0000

    UPDATE MAKEFILE AND CONFIGURE FILES

--- fluid_ht_curvature/config.mk
+++ fluid_ht_curvature/config.mk
@@ -1,4 +1,10 @@
+#path to getfem library
 GETFEM_PREFIX=$(mkGetfemInc)/../
+#flag (yes/no) for debug: DEBUG=no for optimized installation
+DEBUG= no
+#flag (yes/no) for verbose: VERBOSE=yes prints comments at runtime
+VERBOSE = no
+
 ifeq ($(WITH_SAMG),1)
 CXXFLAGS += -I${SAMG}/
 LFLAGS += -L/opt/lib/samg/
