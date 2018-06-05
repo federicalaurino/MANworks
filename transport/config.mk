443541f84b2c021fe4080329cb508aeb84288e9f
Stefano Brambilla <s.brambilla93@gmail.com>
2018-06-05 08:00:31 +0000

    UPDATE MAKEFILE AND CONFIGURE FILES

--- transport/config.mk
+++ transport/config.mk
@@ -1,2 +1,10 @@
+#path to getfem library
 GETFEM_PREFIX=$(mkGetfemInc)/../
+#path to problem3d1d
 PROBLEM_FLUID_PREFIX= /u/brambilla/Desktop/MANworks/fluid_ht_curvature/
+#flag (yes/no) for debug: DEBUG=no for optimized installation
+DEBUG= no
+#flag (yes/no) for verbose: VERBOSE=yes prints comments at runtime
+VERBOSE = no
+
+
