16e4e8527c522c12df8fd21f9d2baa0648fb9a44
Stefano Brambilla <s.brambilla93@gmail.com>
2018-07-12 09:56:52 +0000

    UPDATE CONFIGURATION FILES

--- transport/config.mk
+++ transport/config.mk
@@ -5,5 +5,5 @@ PROBLEM_FLUID_PREFIX= /u/brambilla/Desktop/PACS/MANworks/fluid/
 #flag (yes/no) for debug: DEBUG=no for optimized installation
 DEBUG= no
 #flag (yes/no) for verbose: VERBOSE=yes prints comments at runtime
-VERBOSE = yes
+VERBOSE = no
 
